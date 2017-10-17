1b3b2ba2a49cc06edad5a4ebbf57b0d2a510e514
vmelishkevych <melish127@ukr.net>
2017-10-17 11:25:07 +0300

    update_17/11/17

--- CryptoCurrencyConverter/ServerManager.swift
+++ CryptoCurrencyConverter/ServerManager.swift
@@ -11,84 +11,80 @@ import Foundation
 
 final class ServerManager {
     
-     // Can't init is singleton
-     private init() { }
-     
-     //Shared Instance
-     
-     static let shared = ServerManager()
-     
-     //Instance`s methods
-     
+    // Can't init is singleton
+    private init() { }
+    
+    //Shared Instance
+    
+    static let shared = ServerManager()
+    
+    //Instance`s methods
+    
     func getItems(completion: @escaping ([Currency]?) -> ()) {
         
         
-        // call a closure on a global queue
+        // create URL
         
-        DispatchQueue.global(qos: .userInitiated).async { 
+        guard let url = URL(string: "https://api.coinmarketcap.com/v1/ticker/") else {
+            print("Error: Cannot compose  the URL")
+            completion(nil)
+            return
+        }
+        
+        // load data from web
+        
+        let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
             
-            // create URL
-            
-            guard let url = URL(string: "https://api.coinmarketcap.com/v1/ticker/") else {
-                print("Error: Cannot compose  the URL")
+            guard error == nil else {
+                print("Error while fetching data from Web: \(error!.localizedDescription)")
                 completion(nil)
                 return
             }
-
-            // load data from web
             
-            let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+            guard let dataConfirmed = data else {
+                print("Error: nil data receive from Web")
                 
-                guard error == nil else {
-                    print("Error while fetching data from Web: \(error!.localizedDescription)")
-                    completion(nil)
-                    return
-                }
-                
-                guard let dataConfirmed = data else {
-                    print("Error: nil data receive from Web")
+                if let responseConfirmed = response {
                     
-                    if let responseConfirmed = response {
-                        
-                        debugPrint("Here`s the response: \(responseConfirmed)")
-                    }
-                    completion(nil)
-                    return
+                    debugPrint("Here`s the response: \(responseConfirmed)")
                 }
+                completion(nil)
+                return
+            }
+            
+            // parse data
+            
+            guard let json = try? JSONSerialization.jsonObject(with: dataConfirmed, options: .mutableContainers) else {
+                print("Error parsing JSON")
+                completion(nil)
+                return
+            }
+            
+            //print(json)
+            
+            guard let jsonConfirmed = json as? [[String:Any]] else {
+                print("Error: malformed data in parsed object")
+                completion(nil)
+                return
+            }
+            
+            var itemsArray = [Currency]()
+            
+            for itemDict in jsonConfirmed {
                 
-                // parse data
-                
-                guard let json = try? JSONSerialization.jsonObject(with: dataConfirmed, options: .mutableContainers) else {
-                    print("Error parsing JSON")
-                    completion(nil)
-                    return
-                }
-
-                //print(json)
-                
-                guard let jsonConfirmed = json as? [[String:Any]] else {
-                    print("Error: malformed data in parsed object")
-                    completion(nil)
-                    return
-                }
-                
-                var itemsArray = [Currency]()
-                
-                for itemDict in jsonConfirmed {
+                if let item = Currency(paramsDict: itemDict) {
                     
-                    if let item = Currency(paramsDict: itemDict) {
-                        
-                        itemsArray.append(item)
-                    }
+                    itemsArray.append(item)
                 }
-                
-                DispatchQueue.main.async(execute: {
-                    completion(itemsArray)
-                })
+            }
+            
+            DispatchQueue.main.async(execute: {
+                completion(itemsArray)
             })
-            
-            task.resume()
-            
-        }
+        })
+        
+        task.resume()
+        
     }
+    
 }
